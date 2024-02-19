47161d4e5322238ba8bb20bccd2f9e242cd3989a
afrin <mohdafrinnoushad@gmail.com>
2024-02-19 20:31:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {Feed,ChannelDetail,Navbar,SearchFeed,VideoDetail} from './components/index';
+const App = () => (
+     <BrowserRouter>
+     <Box sx={{backgroundColor:'#000'}}>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Feed />}></Route>
+        <Route path='/youtube-app' exact element={<Feed />}></Route>
+        <Route path='/video/:id' exact element={<VideoDetail />}></Route>
+        <Route path='/channel/:id' exact element={<ChannelDetail />}></Route>
+        <Route path='/search/:searchTerm' exact element={<SearchFeed />}></Route>
+      </Routes>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+     </Box>
+     </BrowserRouter>
+)
 
-export default App;
+export default App
